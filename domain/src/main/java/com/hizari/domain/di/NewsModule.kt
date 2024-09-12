0eeb823d9a1804d09bc5cc0a67bde97ab561471d
hizari <hhizari@gmail.com>
2024-09-12 19:52:50 +0700

    Fix use case

--- domain/src/main/java/com/hizari/domain/di/NewsModule.kt
+++ domain/src/main/java/com/hizari/domain/di/NewsModule.kt
@@ -1,11 +1,13 @@
 package com.hizari.domain.di
 
+import android.content.Context
 import com.hizari.data.repository.NewsRepository
 import com.hizari.domain.usecase.GetNewsBasedOnLatestUseCase
 import com.hizari.domain.usecase.GetNewsBasedOnPopularityUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @InstallIn(SingletonComponent::class)
@@ -13,13 +15,19 @@ import dagger.hilt.components.SingletonComponent
 object NewsModule {
 
     @Provides
-    fun provideGetNewsBasedOnPopularityUseCase(newsRepository: NewsRepository): GetNewsBasedOnPopularityUseCase {
-        return GetNewsBasedOnPopularityUseCase(newsRepository)
+    fun provideGetNewsBasedOnPopularityUseCase(
+        @ApplicationContext context: Context,
+        newsRepository: NewsRepository
+    ): GetNewsBasedOnPopularityUseCase {
+        return GetNewsBasedOnPopularityUseCase(context, newsRepository)
     }
 
     @Provides
-    fun provideGetNewsBasedOnLatestUseCase(newsRepository: NewsRepository): GetNewsBasedOnLatestUseCase {
-        return GetNewsBasedOnLatestUseCase(newsRepository)
+    fun provideGetNewsBasedOnLatestUseCase(
+        @ApplicationContext context: Context,
+        newsRepository: NewsRepository
+    ): GetNewsBasedOnLatestUseCase {
+        return GetNewsBasedOnLatestUseCase(context, newsRepository)
     }
 
 }
--- domain/src/main/java/com/hizari/domain/usecase/GetNewsBasedOnPopularityUseCase.kt
+++ domain/src/main/java/com/hizari/domain/usecase/GetNewsBasedOnPopularityUseCase.kt
@@ -4,12 +4,16 @@ import android.content.Context
 import com.hizari.common.util.Resources
 import com.hizari.data.repository.NewsRepository
 import com.hizari.domain.model.News
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-class GetNewsBasedOnPopularityUseCase(private val newsRepository: NewsRepository) {
+class GetNewsBasedOnPopularityUseCase(
+    @ApplicationContext private val context: Context,
+    private val newsRepository: NewsRepository
+) {
 
-    operator fun invoke(context: Context): Flow<Resources<List<News>?>> = flow {
+    operator fun invoke(): Flow<Resources<List<News>?>> = flow {
         emit(Resources.Loading())
         try {
             val response = newsRepository.getNewsList()
