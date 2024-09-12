24c2b00934e6d550e07c53493cf73eaf29e8e123
hizari <hhizari@gmail.com>
2024-09-12 16:58:21 +0700

    Revert "Revert "Prepare modules""
    This reverts commit 8f502ff273cce29074953ee9def1dbb711d951b6.

---
+++ common/src/main/java/com/hizari/common/MyClass.kt
@@ -0,0 +1,4 @@
+package com.hizari.common
+
+class MyClass {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "Carousell Test"
 include(":app")
- 
+include(":domain")
+include(":data")
+include(":common")
