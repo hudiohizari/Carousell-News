23d4e78a44739daaeb50bee97dee68cec79687a1
hizari <hhizari@gmail.com>
2024-09-12 14:56:59 +0700

    Initial commit

---
+++ app/src/main/java/com/hizari/carouselltest/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.hizari.carouselltest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hizari.carouselltest.ui.screen.MainScreenComposable
+import com.hizari.carouselltest.ui.screen.MainScreenPreview
+import com.hizari.carouselltest.ui.theme.CarousellTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/hizari/carouselltest/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.hizari.carouselltest.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Red = Color(0xFFD32F2F)
+val White = Color(0xFFFFFFFF)
+val Gray = Color(0xFF9E9E9E)
+
+val DarkRed = Color(0xFFB71C1C)
+val DarkGray = Color(0xFF1E1E1E)
+val LightGray = Color(0xFFBDBDBD)
